e33a71ba8a6b165bdea505a7d8135b7dbf702a71
cricrankers <cricrankers@gmail.com>
2026-08-26 19:16:10 +0530

    changed bharatshoppy to bharatshopy

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -3,11 +3,11 @@ import Home from '../pages/home.jsx';
 import Product from '../pages/product.jsx';
 import Store from '../pages/store.jsx';
 import Search from "../pages/search.jsx";
-
+import { Navigate } from "react-router-dom";
 function AppRoutes(){
  return(
     <Routes>
-        <Route path='/' element = {<Home/>}/>
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path='/home' element = {<Home/>}/>
         <Route path="/search" element={<Search />} />
         <Route path="/product/:productId" element={<Product />} />
